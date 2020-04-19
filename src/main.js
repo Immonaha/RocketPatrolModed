6abca293038e01d7e52d44b6087bb668b1faded8
Ian Monahan <immonaha@ucsc.edu>
2020-04-19 12:57:11 -0700

    addded name

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+//IAN MONAHAN
 var config = {
     type: Phaser.CANVAS,
     width: 640,
@@ -15,3 +16,11 @@ game.settings = {
 }
 
 var keyF,keyLEFT,keyRIGHT,keySPACE,keyESC;
+
+// Implement mouse control for player movement and mouse click to fire (25): DONE
+// Create a new title screen (15): DONE
+// Display the time remaining (in seconds) on the screen (15): DONE!
+// Randomize each spaceship's movement direction at the start of each play (10) DONE!
+// Create and implement a new weapon (w/ new behavior and graphics) (25): Done!
+// Track a high score that persists across scenes and display it in the UI (10): DONE
+// Create your own mod and justify its score (??): (added a bunch of stuff. maybe 50 points worth? ;D)
